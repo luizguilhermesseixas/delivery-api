63635a05996c1bcc2f339daa49227c263b801f12
Luiz Guilherme S Seixas <guilhermesschiavon@gmail.com>
2024-03-14 20:28:24 -0300

    Cria testes para stores

--- Gemfile
+++ Gemfile
@@ -46,8 +46,10 @@ gem "bootsnap", require: false
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
-  gem "rspec-rails"
   gem "debug", platforms: %i[ mri windows ]
+  gem "rspec-rails"
+  gem "shoulda-matchers"
+  gem "faker"
 end
 
 group :development do
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Store, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_length_of(:name).is_at_least(3) }
+  end
+
+  let(:valid_attributes) {
+    {name: "Great Restaurant"}
+  }
+
+  let(:invalid_attributes) {
+    {name: nil}
+  }
+
+  describe "GET /index" do
+    it "renders a successful response" do
+      Store.create! valid_attributes
+      get stores_url
+      expect(response).to be_successful
+    end
+  end
+
 end
