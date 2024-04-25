a9936721f06816f7de12221a19b343fd3886a075
Luiz Guilherme S Seixas <guilhermesschiavon@gmail.com>
2024-04-25 16:13:13 -0300

    cria testes para credential

--- spec/models/credential_spec.rb
+++ spec/models/credential_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Credential, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".create_access" do
+    it "raises if the given access role is invalid" do
+      expect {
+        Credential.create_access(:non_existent)
+      }.to raise_error "Invalid Access"
+    end
+
+    it "creates a credential for a given access role" do
+      credential = Credential.create_access(:buyer)
+      expect(credential).to be_buyer
+      expect(credential.key).to_not be_empty
+    end
+  end
 end
